a6857009619227f63a2995b2619ef4cfc04e801b
Marwan <Marwan.kaseer1997@gmail.com>
2022-02-13 23:49:40 +0300

    Program is done

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace for_dongusu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("faktoriyeli hesaplamak istedigin sayiyi giriniz: ");
+            int fak = 1;
+            int sayi = int.Parse(Console.ReadLine());
+            for (int i=1; i<=sayi; i++)
+            {
+                fak *= i;
+            }
+            Console.WriteLine(fak);
+        }
+
+    }
+    
+}
